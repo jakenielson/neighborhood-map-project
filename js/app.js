bcd124b76e4eb1f694fcaa6044313d264729c776
jakenielson <groakayt@gmail.com>
2017-08-24 15:02:02 -0700

    Fit the map when adding markers

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var ViewModel = function () {
   this.geoCoder = null;
   this.placesService = null;
   this.markers = [];
+  this.markerIcon = null;
 
   // The collection of places
   this.places = [];
@@ -19,64 +20,6 @@ var ViewModel = function () {
   this.mode = ko.observable('main');
 
   // Functions
-  // Return to a previous menu
-  this.menuBack = function() {
-    console.log("Meep!");
-    switch (this.mode()){
-      case 'list':
-        // Clear markers
-        if (this.markers) {
-          for (i = 0; i< this.markers.length; i++) {
-            this.markers[i].setMap(null);
-          };
-        };
-        this.markers = [];
-
-        // Change mode
-        this.mode('main');
-        break;
-      default:
-        break;
-    };
-  };
-
-  // Show places
-  this.showPlaces = function() {
-    // Update shownPlaces
-    var shown = [];
-    for (var i = 0; i < 5; i++){
-      shown[i] = this.places[i];
-    };
-    this.shownPlaces(shown);
-
-    // Clear markers
-    if (this.markers) {
-      for (i = 0; i< this.markers.length; i++) {
-        this.markers[i].setMap(null);
-      };
-    };
-
-    this.markers = [];
-
-    // Make new markers
-    for (i = 0; i < this.places.length; i++) {
-      var title = this.places[i].name;
-      var position = this.places[i].geometry.location;
-      var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-
-      marker.setMap(this.map);
-      this.markers.push(marker);
-
-      // Set mode to list
-      this.mode('list');
-    }
-  };
-
   // Get hotels
   this.getHotels = function () {
     this.getPlaces('hotel');
@@ -125,6 +68,60 @@ var ViewModel = function () {
     });
   };
 
+  // Show places on the map and in the list
+  this.showPlaces = function() {
+    // Get the first 5 places
+    var shown = [];
+    for (var i = 0; i < 5; i++){
+      shown[i] = this.places[i];
+    };
+
+    // Show the first 5 places
+    this.shownPlaces(shown);
+
+    // Clear markers
+    this.clearMarkers();
+
+    // Make new markers
+    this.makeMarkers();
+
+    // Set mode to list
+    this.mode('list');
+  };
+
+  // Make new markers
+  this.makeMarkers = function() {
+    var bounds = new google.maps.LatLngBounds();
+    for (i = 0; i < this.places.length; i++) {
+      var title = this.places[i].name;
+      var position = this.places[i].geometry.location;
+      var icon = this.markerIcon;
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        icon: icon,
+        optimize: false,
+        animation: null,
+        id: i
+      });
+      // Add marker to map
+      marker.setMap(this.map);
+      bounds.extend(marker.position);
+      this.markers.push(marker);
+    };
+    this.map.fitBounds(bounds);
+  };
+
+  // Clear all markers
+  this.clearMarkers = function() {
+    if (this.markers) {
+      for (var i = 0; i < this.markers.length; i++) {
+        this.markers[i].setMap(null);
+      };
+    };
+    this.markers = [];
+  };
+
   // Move the map to a new latLng
   this.moveMap = function () {
     self = this;
@@ -135,6 +132,19 @@ var ViewModel = function () {
     });
   };
 
+  // Return to a previous menu
+  this.menuBack = function() {
+    console.log("Meep!");
+    switch (this.mode()){
+      case 'list':
+        this.clearMarkers();
+        this.mode('main');
+        break;
+      default:
+        break;
+    };
+  };
+
   // Initialize the placesService
   this.initPlacesService = function() {
     this.placesService = new google.maps.places.PlacesService(this.map);
@@ -143,6 +153,18 @@ var ViewModel = function () {
   // Initialize the geoCoder
   this.initGeoCoder = function() {
     this.geoCoder = new google.maps.Geocoder();
+  };
+
+  // Initialize the map icon
+  this.initIcon = function() {
+    var markerColor = '0091ff';
+    this.markerIcon = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34));
   };
 
   // Initialize the map
@@ -157,6 +179,7 @@ var ViewModel = function () {
   // Initialize the app
   this.init = function() {
     this.initMap();
+    this.initIcon();
     this.initGeoCoder();
     this.initPlacesService();
   }
